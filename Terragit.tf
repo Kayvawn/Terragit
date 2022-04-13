fd20da9ba644c932ad0bfd3faf71e504e37c7ff0
Kayvawn <86017016+Kayvawn@users.noreply.github.com>
2022-04-12 22:55:01 -0400

    Basic Template with 3 Instances

---
+++ Terragit.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source   = "hashicorp/aws"
+      version = "~> 4.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "FirstInst" {
+ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "Terrakp"
+
+  tags = {
+    "Name" = "InstanceA"
+  }
+  
+}
+
+resource "aws_instance" "SecInst" {
+ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "Terrakp"
+
+  tags = {
+    "Name" = "InstanceB"
+  }
+  
+}
+
+resource "aws_instance" "ThirdInst" {
+ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "Terrakp"
+
+  tags = {
+    "Name" = "InstanceC"
+  }
+  
+}
